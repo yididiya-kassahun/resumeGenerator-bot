7308c4c713fac7a4746537ad555c82ea494f618f
yididiya <57259174+yididiya-kassahun@users.noreply.github.com>
2023-10-31 12:06:06 +0300

    qr code generate done

--- app.js
+++ app.js
@@ -1,19 +1,22 @@
-const express = require('express');
-const mongoose = require('mongoose');
+const express = require("express");
+const mongoose = require("mongoose");
+const fs = require("fs");
+const axios = require("axios");
+const qr = require('qrcode');
 const app = express();
-const port = 3000; 
+const port = 3000;
 
-const TelegramBot = require('node-telegram-bot-api');
+const TelegramBot = require("node-telegram-bot-api");
 
-// controller 
-const resumeController = require('./controllers/resume');
+// controller
+const resumeController = require("./controllers/resume");
 // Models
-const ResumeDetail = require('./models/resumeDetail');
+const ResumeDetail = require("./models/resumeDetail");
 
 // Routes
-const resumeRouter = require('./routes/resume');
+const resumeRouter = require("./routes/resume");
 
-app.use(express.static('public'));
+app.use(express.static("public"));
 
 app.set("view engine", "ejs");
 app.set("views", "views");
@@ -21,191 +24,356 @@ app.set("views", "views");
 app.use(resumeRouter);
 app.use(ResumeDetail);
 
-const TOKEN = process.env.TELEGRAM_TOKEN || '6842036257:AAFvITVr60QqxmOGDWlu_a_wooSwbY3XTaI';
-
+const TOKEN =
+  process.env.TELEGRAM_TOKEN ||
+  "6842036257:AAFvITVr60QqxmOGDWlu_a_wooSwbY3XTaI";
 
 // ************* Bot
 const bot = new TelegramBot(TOKEN, { polling: true });
 
 bot.onText(/start/, (msg) => {
-    startMenu(msg);
-  });
-
-function startMenu(msg) {
-    const opts = {
-      reply_markup: JSON.stringify({
-        keyboard: [
-          [{ text: "👨🏽‍💻 Generate Resume" }, { text: "👨🏽‍💻 developer" }],
-          [{text:"🗑️ Delete Resume"},{ text: "🔙 Back" }],
-        ],
-        resize_keyboard: true,
-        one_time_keyboard: true,
-      }),
-    };
-    bot.sendMessage(msg.chat.id, " start menu ", opts);
-}
-
-bot.onText(/Generate Resume/,(msg)=> {
-    askDetails(msg);
+  startMenu(msg);
 });
 
-bot.onText(/Delete Resume/,(msg)=> {
-    deleteResume(msg);
+function startMenu(msg) {
+  const opts = {
+    reply_markup: JSON.stringify({
+      keyboard: [
+        [{ text: "👨🏽‍💻 Generate Resume" }, { text: "👨🏽‍💻 developer" }],
+        [{ text: "🗑️ Delete Resume" }, { text: "🔙 Back" }],
+      ],
+      resize_keyboard: true,
+      one_time_keyboard: true,
+    }),
+  };
+  bot.sendMessage(
+    msg.chat.id,
+    " start menu \n\n If you here created a resume you have to delete it first ",
+    opts
+  );
+}
+
+bot.onText(/Generate Resume/, (msg) => {
+  askDetails(msg);
+});
+
+bot.onText(/Delete Resume/, (msg) => {
+  deleteResume(msg);
 });
 
 // Define a conversation state object to store user inputs
 const conversationState = {};
 
-function askDetails(msg){
-    conversationState[msg.chat.id] = {
-        step: 1, // Current step in the conversation
-        fullName: "",
-        jobTitle:"",
-        age:"",
-        email:"",
-        phone:"",
-        address:""
-    };
+function askDetails(msg) {
+  conversationState[msg.chat.id] = {
+    step: 1, // Current step in the conversation
+    photoURL: "",
+    fullName: "",
+    jobTitle: "",
+    age: "",
+    email: "",
+    phone: "",
+    address: "",
+  };
 
-    askFullName(msg.chat.id);
+  askPhoto(msg.chat.id);
 }
-
+function askPhoto(chatId) {
+  bot.sendMessage(chatId, "Enter your photo NB: 500px by 500px:", {
+    reply_markup: { force_reply: true },
+  });
+}
 function askFullName(chatId) {
-    bot.sendMessage(chatId, "Enter your full name:", {
-        reply_markup: { force_reply: true },
-    });
+  bot.sendMessage(chatId, "Enter your full name:", {
+    reply_markup: { force_reply: true },
+  });
 }
 
 function askJobTitle(chatId) {
-    bot.sendMessage(chatId, "Enter job title:", {
-        reply_markup: { force_reply: true },
-    });
+  bot.sendMessage(chatId, "Enter job title:", {
+    reply_markup: { force_reply: true },
+  });
 }
 
 function askAge(chatId) {
-    bot.sendMessage(chatId, "Enter your Age:", {
-        reply_markup: { force_reply: true },
-    });
+  bot.sendMessage(chatId, "Enter your Age:", {
+    reply_markup: { force_reply: true },
+  });
 }
 function askEmail(chatId) {
-    bot.sendMessage(chatId, "Enter your email:", {
-        reply_markup: { force_reply: true },
-    });
+  bot.sendMessage(chatId, "Enter your email:", {
+    reply_markup: { force_reply: true },
+  });
 }
 function askPhone(chatId) {
-    bot.sendMessage(chatId, "Enter your phone number:", {
-        reply_markup: { force_reply: true },
-    });
+  bot.sendMessage(chatId, "Enter your phone number:", {
+    reply_markup: { force_reply: true },
+  });
 }
 
 function askAddress(chatId) {
-    bot.sendMessage(chatId, "Enter your Address:", {
-        reply_markup: { force_reply: true },
+  bot.sendMessage(chatId, "Enter your Address:", {
+    reply_markup: { force_reply: true },
+  });
+}
+
+let photoPath;
+let photoPathPromise;
+
+bot.on("photo", (msg) => {
+  const chatId = msg.chat.id;
+  const photoId = msg.photo[0].file_id; // Get the file ID of the photo
+  const caption = msg.caption || ""; // Get the caption (if any)
+
+  // If you want to download or process the photo, you can use the getFile method
+  bot.getFile(photoId).then((fileInfo) => {
+    const fileUrl = `https://api.telegram.org/file/bot${TOKEN}/${fileInfo.file_path}`;
+
+    console.log("fileUrl " + fileUrl);
+    const fileUrlWithSize = fileUrl.replace("file.jpg", "file_0.jpg");
+    // Download the photo using axios
+    axios({
+      method: "get",
+      url: fileUrlWithSize,
+      responseType: "stream",
+    }).then((response) => {
+      // Specify the path where you want to save the downloaded photo
+      photoPath = __dirname + `/public/photos/${chatId}.jpg`;
+
+      // Create a writable stream to save the file
+      const writer = fs.createWriteStream(photoPath);
+
+      // Pipe the response stream to the writer
+      response.data.pipe(writer);
+
+      writer.on("finish", () => {
+        console.log("Photo downloaded and saved at", photoPath);
+
+        // Resolve the photoPathPromise when the photo has been saved
+        if (photoPathPromise) {
+          photoPathPromise.resolve(photoPath);
+        }
+      });
+
+      writer.on("error", (error) => {
+        console.error("Error downloading photo:", error);
+
+        // Reject the photoPathPromise if there's an error
+        if (photoPathPromise) {
+          photoPathPromise.reject(error);
+        }
+      });
     });
+  });
+});
+
+// Define a function to get the photoPath when it's available
+function getPhotoPath() {
+  if (photoPath) {
+    // If photoPath is already available, return it immediately
+    return Promise.resolve(photoPath);
+  } else {
+    // If photoPath is not available yet, create a promise for it
+    if (!photoPathPromise) {
+      photoPathPromise = {};
+      photoPathPromise.promise = new Promise((resolve, reject) => {
+        photoPathPromise.resolve = resolve;
+        photoPathPromise.reject = reject;
+      });
+    }
+    return photoPathPromise.promise;
+  }
 }
 
 // Handle user replies
-bot.on('message', (msg) => {
-    const chatId = msg.chat.id;
-    const userState = conversationState[chatId];
+bot.on("message", (msg) => {
+  const chatId = msg.chat.id;
+  const userState = conversationState[chatId];
 
-    if (userState) {
+  getPhotoPath()
+    .then((path) => {
+      if (userState) {
         switch (userState.step) {
-            case 1:
-                userState.fullName = msg.text;
-                userState.step++;
-                askJobTitle(chatId);
-                break;
-            case 2:
-                userState.jobTitle = msg.text;
-                userState.step++;
-                askAge(chatId);
-                break;
-            case 3:
-                userState.age = msg.text;
-                userState.step++;
-                askEmail(chatId);
-                break;
-            case 4:
-                userState.email = msg.text;
-                userState.step++;
-                askPhone(chatId);
-                break;
-            case 5:
-                userState.phone = msg.text;
-                userState.step++;
-                askAddress(chatId);
-                break;
-            case 6:
-                userState.address = msg.text;
-                userState.step++;
- 
-                // You now have all the user inputs in userState
-                const { fullName,jobTitle,age,email,phone,address} = userState;
-                console.log(fullName,jobTitle,age,email,phone,address);
-                const dataResult = addResumeDetail(chatId,fullName, jobTitle,age, email, phone,address);
-                
-                if(!dataResult){
-                    bot.sendMessage(msg.chat.id,"profile exist. please delete it to create another");
-                }else{
-                    const link = `https://a0b5-196-191-190-41.ngrok-free.app/get.resume/${chatId}`; // test it with ngrok
-                    bot.sendMessage(msg.chat.id, `<a href="${link}">Check out this link</a>`, { parse_mode: 'HTML'});
-                }
+          case 1:
+            userState.photoURL = path;
+            userState.step++;
+            askFullName(chatId);
+            break;
+          case 2:
+            userState.fullName = msg.text;
+            userState.step++;
+            askJobTitle(chatId);
+            break;
+          case 3:
+            userState.jobTitle = msg.text;
+            userState.step++;
+            askAge(chatId);
+            break;
+          case 4:
+            userState.age = msg.text;
+            userState.step++;
+            askEmail(chatId);
+            break;
+          case 5:
+            userState.email = msg.text;
+            userState.step++;
+            askPhone(chatId);
+            break;
+          case 6:
+            userState.phone = msg.text;
+            userState.step++;
+            askAddress(chatId);
+            break;
+          case 7:
+            userState.address = msg.text;
+            userState.step++;
 
-                break;  
+            // You now have all the user inputs in userState
+            const { photoURL, fullName, jobTitle, age, email, phone, address } =
+              userState;
+            console.log(
+              "console log ... " + photoURL,
+              fullName,
+              jobTitle,
+              age,
+              email,
+              phone,
+              address
+            );
+            const dataResult = addResumeDetail(
+              chatId,
+              photoURL,
+              fullName,
+              jobTitle,
+              age,
+              email,
+              phone,
+              address
+            );
+
+            if (dataResult) {
+              bot.sendMessage(
+                msg.chat.id,
+                "profile exist. please delete it to create another"
+              );
+            } else {
+              const link = `https://3ba7-196-189-243-16.ngrok-free.app/get.resume/${chatId}`; // test it with ngrok
+
+              bot.sendMessage(
+                msg.chat.id,
+                `Congradulation 🎉 Your Resume profile is Ready! \n\nclick the link or scan the QR Code \n<a href="${link}">Check out this link</a>`,
+                { parse_mode: "HTML" }
+              );
+
+              // generate qr code for the link
+              let data = {
+                "link":`${link}`
+              };
+              let filename = 'qrCode.png';
+
+              let stJson = JSON.stringify(data);
+              qr.toDataURL(stJson,function(err,base64Data){
+                if(err)return console.log(err);
+                //console.log(code);
+                sendBase64File(chatId, base64Data, filename);
+              })
+
+            }
+
+            break;
         }
-    }
+      }
+    })
+    .catch((error) => {
+      console.error(error);
+    });
 });
 
 bot.onText(/developer/, (msg) => {
-    bot.sendMessage(msg.chat.id, "Developed By: Yididiya Kassahun\n\n ");
-  });
-  
+  bot.sendMessage(msg.chat.id, "Developed By: Yididiya Kassahun\n\n ");
+});
+
 bot.onText(/back/, (msg) => {
-    startMenu(msg);
+  startMenu(msg);
+});
+
+function addResumeDetail(
+  chatId,
+  photoURL,
+  fullName,
+  jobTitle,
+  age,
+  email,
+  phone,
+  address
+) {
+  const ChatId = chatId.toString();
+  const PhotoURL = photoURL.toString();
+  const FullNamee = fullName.toString();
+  const JobTitle = jobTitle.toString();
+  const Age = age.toString();
+  const Email = email.toString();
+  const Phone = phone.toString();
+  const Address = address.toString();
+
+  console.log("full name" + FullNamee);
+
+  let state;
+
+  ResumeDetail.findOne({ chatId: ChatId }).then((result) => {
+    if (result) {
+      return (state = true);
+    } else {
+      const details = new ResumeDetail({
+        chatId: ChatId,
+        photoURL: PhotoURL,
+        fullName: FullNamee,
+        jobTitle: JobTitle,
+        age: Age,
+        email: Email,
+        phone: Phone,
+        address: Address,
+      });
+
+      details.save();
+      return (state = false);
+    }
   });
-
-function addResumeDetail(chatId,fullName,jobTitle,age,email,phone,address){
-    const ChatId = chatId.toString();
-    const FullName = fullName.toString();
-    const JobTitle = jobTitle.toString();
-    const Age = age.toString();
-    const Email = email.toString();
-    const Phone = phone.toString();
-    const Address = address.toString();
-
-    let state;
-
-    ResumeDetail.findOne({chatId:ChatId})
-    .then(result=>{
-        if(result){
-            return state = true;
-        }else{
-            const details = new ResumeDetail({
-                chatId:ChatId,
-                fullName:FullName,
-                jobTitle:JobTitle,
-                age:Age,
-                email:Email,
-                phone:Phone,
-                address:Address
-             })
-          
-             details.save();
-             return state = false;
-        }
-    })
-  
 }
 
-function deleteResume(chatId){
+// Function to send a base64 file
+function sendBase64File(chatId, base64Data, filename) {
+  const base64Image = base64Data.replace(/^data:image\/\w+;base64,/, '');
+  const dataBuffer = Buffer.from(base64Image, 'base64');
+
+    // Save the base64 data to a temporary image file
+    const tempImagePath = `./temp/${filename}`;
+    fs.writeFileSync(tempImagePath, dataBuffer);
+  
+  // Send the temporary image file as a photo
+  bot.sendPhoto(chatId, tempImagePath)
+    .then(() => {
+      // Optional: Delete the temporary image file after sending
+      fs.unlinkSync(tempImagePath);
+    })
+    .catch((error) => {
+      console.error('Error sending base64 image as a photo:', error);
+    });
+}
+
+function deleteResume(chatId) {
   //  ResumeDetail.findOne({chat})
 }
 
-mongoose.connect('mongodb+srv://yididiya:mnYxZgzDQhULh2Ow@cluster0.de0jhxk.mongodb.net/resume-DB?retryWrites=true&w=majority').then(result=> {
-  app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
+mongoose
+  .connect(
+    "mongodb+srv://yididiya:mnYxZgzDQhULh2Ow@cluster0.de0jhxk.mongodb.net/resume-DB?retryWrites=true&w=majority"
+  )
+  .then((result) => {
+    app.listen(port, () => {
+      console.log(`Server is running on port ${port}`);
+    });
+  })
+  .catch((err) => {
+    console.log(err);
   });
-}).catch(err=> {
-  console.log(err);
-})
--- models/resumeDetail.js
+++ models/resumeDetail.js
@@ -7,6 +7,10 @@ const resumeSchema = new Schema({
     type: String,
     required:true
     },
+    photoURL:{
+    type: String,
+    required:true
+    },
    fullName: {
     type: String,
     required:true
